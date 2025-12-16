a8c9910cfb5bc50ba388d9e9d0d3f896e48058ab
NicolasLizama23 <nicolowi@gmail.com>
2025-12-16 01:23:34 -0300

    Guard PyMySQL import when DATABASE_URL is set

--- iconstruction_project/__init__.py
+++ iconstruction_project/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-# Solo habilitar PyMySQL cuando estés usando MySQL (local/XAMPP)
+# Solo usar PyMySQL cuando NO haya DATABASE_URL (o sea, en local con MySQL/XAMPP)
 if not os.getenv("DATABASE_URL"):
     import pymysql
     pymysql.install_as_MySQLdb()
